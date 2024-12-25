38af54a5cd41a7837005bfd5ee1fc11d63b5e518
JeongSeoJin <pysuk88@gmail.com>
2024-12-25 16:23:45 +0900

    Add : voltage_control Update : angle_control

--- voltage_control/voltage_control.ino
+++ voltage_control/voltage_control.ino
@@ -1,17 +1,16 @@
 /**
- *
- * Position/angle motion control example
- * Steps:
- * 1) Configure the motor and magnetic sensor
- * 2) Run the code
- * 3) Set the target angle (in radians) from serial terminal
- *
+ * Torque control example using voltage control loop.
+ * 
+ * Most of the low-end BLDC driver boards doesn't have current measurement therefore SimpleFOC offers 
+ * you a way to control motor torque by setting the voltage to the motor instead hte current. 
+ * 
+ * This makes the BLDC motor effectively a DC motor, and you can use it in a same way.
  */
 #include <SimpleFOC.h>
 
 // magnetic sensor instance - SPI
 // MagneticSensorSPI sensor = MagneticSensorSPI(AS5147_SPI, 10);
-// magnetic sensor instance - MagneticSensorI2C
+// magnetic sensor instance - I2C
 MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 // magnetic sensor instance - analog output
 // MagneticSensorAnalog sensor = MagneticSensorAnalog(A1, 14, 1020);
@@ -23,11 +22,11 @@ BLDCDriver3PWM driver = BLDCDriver3PWM(9, 5, 6, 8);
 //StepperMotor motor = StepperMotor(50);
 //StepperDriver4PWM driver = StepperDriver4PWM(9, 5, 10, 6,  8);
 
-// angle set point variable
-float target_angle = 0;
+// voltage set point variable
+float target_voltage = 2;
 // instantiate the commander
 Commander command = Commander(Serial);
-void doTarget(char* cmd) { command.scalar(&target_angle, cmd); }
+void doTarget(char* cmd) { command.scalar(&target_voltage, cmd); }
 
 void setup() {
 
@@ -42,41 +41,20 @@ void setup() {
   // link the motor to the sensor
   motor.linkSensor(&sensor);
 
-  // driver config
-  // power supply voltage [V]
+  // power supply voltage
   driver.voltage_power_supply = 12;
   driver.init();
-  // link the motor and the driver
   motor.linkDriver(&driver);
 
+  // aligning voltage 
+  motor.voltage_sensor_align = 5;
   // choose FOC modulation (optional)
   motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
-
   // set motion control loop to be used
-  motor.controller = MotionControlType::angle;
+  motor.controller = MotionControlType::torque;
 
-  // contoller configuration
-  // default parameters in defaults.h
-
-  // velocity PI controller parameters
-  motor.PID_velocity.P = 0.1f;
-  motor.PID_velocity.I = 1.0;
-  motor.PID_velocity.D = 0;
-  // maximal voltage to be set to the motor
-  motor.voltage_limit = 6;
-
-  // velocity low pass filtering time constant
-  // the lower the less filtered
-  motor.LPF_velocity.Tf = 0.01f;
-
-  // angle P controller
-  motor.P_angle.P = 20;
-  // maximal velocity of the position control
-  motor.velocity_limit = 10;
-  
   // comment out if not needed
   motor.useMonitoring(Serial);
-
 
   // initialize motor
   motor.init();
@@ -84,33 +62,27 @@ void setup() {
   motor.initFOC();
 
   // add target command T
-  command.add('T', doTarget, "target angle");
+  command.add('T', doTarget, "target voltage");
 
   Serial.println(F("Motor ready."));
-  Serial.println(F("Set the target angle using serial terminal:"));
+  Serial.println(F("Set the target voltage using serial terminal:"));
   _delay(1000);
 }
-
 
 void loop() {
 
   // main FOC algorithm function
   // the faster you run this function the better
   // Arduino UNO loop  ~1kHz
-  // Bluepill loop ~10kHz
+  // Bluepill loop ~10kHz 
   motor.loopFOC();
 
   // Motion control function
   // velocity, position or voltage (defined in motor.controller)
   // this function can be run at much lower frequency than loopFOC() function
   // You can also use motor.move() and set the motor.target in the code
-  motor.move(target_angle);
-
-
-  // function intended to be used with serial plotter to monitor motor variables
-  // significantly slowing the execution down!!!!
-  // motor.monitor();
-
+  motor.move(target_voltage);
+  
   // user communication
   command.run();
 }
